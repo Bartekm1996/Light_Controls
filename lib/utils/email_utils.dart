dd9f74ac725c436bb578cae89d62d832eb6d1a91
Bartekm1996 <35643353+Bartekm1996@users.noreply.github.com>
2022-05-03 10:24:21 +0100

    Update email_utils.dart

--- lib/utils/email_utils.dart
+++ lib/utils/email_utils.dart
@@ -3,9 +3,9 @@ part of utils;
 class EmailUtils{
 
   static void sendEmail(String exception, String stackTrace){
-    final mailer = Mailer('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');
-    final toAddress = Address('bmlynarkiewicz1996@gmail.com');
-    final fromAddress = Address('lightscontrol2021@gmail.com');
+    final mailer = Mailer('');
+    final toAddress = Address('');
+    final fromAddress = Address('');
     final content = Content('text/plain', stackTrace);
     final subject = 'Lights Control - ' + exception;
     final personalization = Personalization([toAddress]);
